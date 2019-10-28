4fa0d809f4446edbc118f4eedf14f9a61c84f173
Titus Ouko <titusouko@gmail.com>
2019-10-28 18:02:41 +0300

    Add subscribe class to model

--- app/models.py
+++ app/models.py
@@ -127,4 +127,7 @@ class Comment(db.Model):
         Comment.all_comments.clear()
 
 
-
+class Subscribe(db.Model):
+    __tablename__ = 'subscribers'
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(50))
